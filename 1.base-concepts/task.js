cf2f61ce4b03be73a866a945339f9361666fe812
Surik95 <pokemon.surik@gmail.com>
2022-06-09 12:39:48 +0600

    Выполнил первое задание, решение уровнения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,25 @@
+'use strict';
+
+// этот код работает в современном режиме
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  arr = [];
+  
+  let discriminant = b ** 2 - 4 * a * c;
+  console.log(discriminant)
+  
+  if (discriminant < 0) {
+    arr = []
+  } else if (discriminant === 0) {
+    arr.push(-b/(2*a));
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant) )/(2*a));
+    arr.push((-b - Math.sqrt(discriminant) )/(2*a));
+  }
+
+
   return arr; // array
 }
 

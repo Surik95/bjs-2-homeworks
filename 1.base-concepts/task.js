b9c404e6c4dcca501cc1ac861b0b8e290eb86767
Surik95 <pokemon.surik@gmail.com>
2022-06-10 22:39:03 +0600

    Выполнил второе задание, калькулятор уровнения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -23,10 +23,54 @@ function solveEquation(a, b, c) {
   return arr; // array
 }
 
+//код для задачи № 2
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  //привожу все значение к цифровому формату  
+  let percentNumber =  Number(percent) / 100
+  let contributionNumber = Number(contribution)
+  let amountNumber = Number(amount)
 
-  // код для задачи №2 писать здесь
+  //проверяю что значения являются цифрами
+  if (isNaN(percentNumber)) {
+    totalAmount = `Параметр проценты содержит неправильное значение "${percent}"`
+    return totalAmount
+  } else if (isNaN(contributionNumber)) {
+    totalAmount = `Параметр первоначальный взнос содержит неправильное значение "${contribution}"`
+    return totalAmount
+  } else if (isNaN(amountNumber)) {
+    totalAmount = `Параметр сумма кредита содержит неправильное значение "${amount}"`
+    return totalAmount
+  }
 
-  return totalAmount;
+  //определяю ткло кредита
+
+  let loanBodu = amountNumber - contributionNumber;
+
+  //определяю кол-во месяцев
+
+  let loanTerm;
+  let today = new Date;
+  let differenceMonth = date.getMonth() - today.getMonth();
+  let differenceYear = date.getFullYear() - today.getFullYear();
+
+  //проверяю что не указана неккоректная дата
+
+  if (differenceYear < 0 ) {
+    totalAmount = `Параметр срок ипотеки содержит неправильное значение`
+    return totalAmount
+  } else {
+    loanTerm = differenceYear * 12 + differenceMonth}
+  
+  //рассчитываю ежемесячный платеж
+
+  percentNumber =  percentNumber / 12;
+  let monthlyPayment = loanBodu * (percentNumber + (percentNumber / (((1 + percentNumber)**loanTerm) - 1))); 
+ 
+  //рассчитываю сумму кредита
+ 
+  totalAmount = monthlyPayment * loanTerm
+
+  return totalAmount.toFixed(2);
 }

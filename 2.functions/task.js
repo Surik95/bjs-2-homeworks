49a518b6c3d311151e66509b878e8451423f431c
Surik95 <pokemon.surik@gmail.com>
2022-06-13 14:02:58 +0600

    Исправил указанные ошибки по дз тема 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -7,14 +7,18 @@ function getArrayParams(arr) {
   max = arr[0];
   sum = 0;
 
-  for (i = 0; i < arr.length; i +=1) {
-      min > arr[i] ? min = arr[i] : min;
-      max < arr[i] ? max = arr[i] : max;
-      sum += arr[i]    
+  for (let i = 0; i < arr.length; i +=1) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    sum += arr[i];   
   }
   
   avg = sum / arr.length;
-  avg = parseFloat(avg.toFixed(2))
+  avg = parseFloat(avg.toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
@@ -24,8 +28,8 @@ function worker(arr) {
   let sum = 0;
 
   // Ваш код
-  for (i = 0; i < arr.length; i +=1) {
-      sum += arr[i]    
+  for (let i = 0; i < arr.length; i +=1) {
+      sum += arr[i];   
   }
   return sum;
 }
@@ -34,10 +38,12 @@ function makeWork(arrOfArr, func) {
   let max;
 
   // Ваш кода
-    max = -Infinity
-    for (k = 0; k < arrOfArr.length; k +=1){
-        sum = func(arrOfArr[k]);
-        sum > max ? max = sum : max;
+    max = -Infinity;
+    for (let k = 0; k < arrOfArr.length; k +=1){
+      sum = func(arrOfArr[k]);
+      if (sum > max) {
+        max = sum;
+      }
     }    
   
   return max;
@@ -46,23 +52,18 @@ function makeWork(arrOfArr, func) {
 // Задание 3
 function worker2(arr) {
   // Ваш код
-    let min = Infinity
-    let max = -Infinity
+    let min = Infinity;
+    let max = -Infinity;
     let modal = 0;
 
-    for (i = 0; i < arr.length; i +=1) {
-        min > arr[i] ? min = arr[i] : min;
-        max < arr[i] ? max = arr[i] : max;
+    for (let i = 0; i < arr.length; i +=1) {
+      if (min > arr[i]) {
+        min = arr[i];
+      }
+      if (max < arr[i]) {
+        max = arr[i];
+      }
     }
-        
-    if (min >= 0 && max >= 0) {
-        modal = max - min;
-        return Math.abs(modal);
-    } else if (min <=0 && max <= 0) {
-        modal = min - max;
-        return Math.abs(modal);
-    } else {
-        modal = Math.abs(max) + Math.abs(min);
-        return modal;
-    }
+    
+  return modal = Math.abs(max - min);
 }

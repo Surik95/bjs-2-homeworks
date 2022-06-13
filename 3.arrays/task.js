60f8b55c4e4c4f53425649b1c03660c5e3f0344c
Surik95 <pokemon.surik@gmail.com>
2022-06-13 23:12:09 +0600

    Решение заданий 1,2 тема 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,8 +1,8 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+  
   // Ваш код
-
+  arr1.every((item,index) => arr1.length === arr2.length && arr1[index] === arr2[index] ) ? result = true : result = false;
   return result; // boolean
 }
 
@@ -10,6 +10,6 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+  resultArr = arr.filter(item => (item > 0)).filter(item => item % 3 === 0).map(item => item * 10) 
   return resultArr; // array
 }

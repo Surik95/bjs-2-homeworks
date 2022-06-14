0743ad28c8930ac3346e1f976a73fac100063ade
Surik95 <pokemon.surik@gmail.com>
2022-06-15 00:33:04 +0600

    Исправил указанные недостатки в дз по теме №3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,14 +2,13 @@ function compareArrays(arr1, arr2) {
   let result;
   
   // Ваш код
-  arr1.every((item,index) => arr1.length === arr2.length && arr1[index] === arr2[index] ) ? result = true : result = false;
+  if (arr1.length === arr2.length) {
+    result = arr1.every((item,index) => item[index] === item[index] ) ? true :  false;
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
   // Ваш код
-  resultArr = arr.filter(item => (item > 0)).filter(item => item % 3 === 0).map(item => item * 10) 
-  return resultArr; // array
+  return arr.filter(item => (item > 0)).filter(item => item % 3 === 0).map(item => item * 10)  // array
 }

90fe7ba94c7838a2fb57350bef7e40f7ae921302
Surik95 <pokemon.surik@gmail.com>
2022-06-19 15:05:08 +0600

    Решение домашнего задания по теме 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
     // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+  Student.prototype.addMark = function(mark) {
+    //ваш код
+    this.marks;
+    if(this.marks === undefined){ 
+      // добавить первую оценку 
+      this.marks = [mark]
+      } else {
+        // добавить вторую и последующие оценки в массив
+        this.marks.push(mark)
+      }
+  }
+
+  Student.prototype.addMarks = function(...mark) {
+    //ваш код
+    if(this.marks === undefined){ 
+      // добавить первую оценку 
+      this.marks = mark
+      } else {
+        // добавить вторую и последующие оценки в массив
+        this.marks = this.marks.contact(mark);
+      }
+  }
+
+  Student.prototype.getAverage = function() {
+    let sum = this.marks.reduce((acc, item) => item + acc, 0);
+    return sum/this.marks.length
+  }
+
+  Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason
+  }

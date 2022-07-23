f8b319ee673146bfcd0347ac42fc7851de97279b
Surik95 <pokemon.surik@gmail.com>
2022-07-23 12:14:53 +0600

    Выполнил домашнее задание по теме 8 v2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -31,6 +31,37 @@ function debounceDecoratorNew(func, delay) {
   let timeCall;
   let timePreviousCall = 0
   
+  return function (...args){
+    timeCall = Math.floor(performance.now() - timeStart)
+    if (flug === null) {
+    console.log(func())
+    flug = false  
+    timeId = setTimeout(() => flug = true, 2000)
+    } else if (!flug) {
+      if (timePreviousCall < 99) {
+        timePreviousCall = 0
+        console.log(`проигнорировано так как от последнего вызова прошло менее 2000мс (${timeCall} - ${timePreviousCall} < 2000)`)
+      } else {
+      console.log(`проигнорировано так как от последнего вызова прошло ${timeCall}-${timePreviousCall}=${timeCall - timePreviousCall} (${timeCall - timePreviousCall} < 2000)`)
+      clearTimeout(timeId);
+      timeId = setTimeout(() => flug = true, 2000)
+      }
+    }
+    else if (flug) {
+    setTimeout(() => console.log(func(...args)), delay)
+    }
+    timePreviousCall = timeCall
+  }
+}
+
+function debounceDecorator2(func) {
+  // Ваш код
+  let flug = null;
+  let timeId = null;
+  let timeStart = performance.now();
+  let timeCall;
+  let timePreviousCall = 0
+  
   function wrapper(...args){
     timeCall = Math.floor(performance.now() - timeStart)
     if (flug === null) {
@@ -55,8 +86,4 @@ function debounceDecoratorNew(func, delay) {
   }
   wrapper.count = 0
   return wrapper
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
 }
